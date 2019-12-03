a8c5b6684bf114e218cd86d96e0f0b747229f72f
kenAlparslan <47366236+aslanken@users.noreply.github.com>
2019-12-03 10:43:16 -0500

    implemented the functionality for combining multiple orders from the same customer

--- WallyWorld/Cart_Page.xaml.cs
+++ WallyWorld/Cart_Page.xaml.cs
@@ -21,9 +21,18 @@ namespace WallyWorld
     /// </summary>
     public partial class Cart_Page : Page
     {
+        private string customerName;
+        private List<string> ProductName;
+        private int quantity;
+        private string branchName;
+        private decimal cost;
+        private List<string> sessionID;
+
         public Cart_Page()
         {
             InitializeComponent();
+            ProductName = new List<string>();
+            sessionID = new List<string>();
         }
 
         private void Show_Cart_Click(object sender, RoutedEventArgs e)
@@ -38,7 +47,7 @@ namespace WallyWorld
 
                 FrameworkElementFactory factory1 = new FrameworkElementFactory(typeof(Button));
                 factory1.SetValue(Button.ContentProperty, "Checkout");
-                factory1.AddHandler(Button.ClickEvent, new RoutedEventHandler(SelectBtn_Click));
+                factory1.AddHandler(Button.ClickEvent, new RoutedEventHandler(CheckoutBtn_Click));
                 DataTemplate cellTemplate1 = new DataTemplate();
                 cellTemplate1.VisualTree = factory1;
                 col1.CellTemplate = cellTemplate1;
@@ -49,7 +58,68 @@ namespace WallyWorld
             Cart.ItemsSource = dt.DefaultView;
         }
 
-        private void SelectBtn_Click(object sender, RoutedEventArgs e)
+        private void CheckoutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView r;
+            if (totalP.IsVisible == false)
+            {
+                r = (DataRowView)((Button)e.Source).DataContext;
+                int id = int.Parse(r["SKU"].ToString());
+                string cName = r["customerName"].ToString();
+                string pName = r["productName"].ToString();
+                string bName = r["branchName"].ToString();
+                int totalStock = int.Parse(r["quantity"].ToString());
+                decimal price = decimal.Parse(r["price"].ToString(), System.Globalization.NumberStyles.Currency);
+
+                sessionID.Add(r["sessionID"].ToString());
+                customerName = cName;
+                ProductName.Add(pName);
+                branchName = bName;
+                quantity = totalStock;
+                cost = price;
+                totalP.Text = "Total: $" + cost.ToString();
+                totalP.Visibility = Visibility.Visible;
+
+            }
+            else
+            {
+                r = (DataRowView)((Button)e.Source).DataContext;
+                if (sessionID.Contains(r["sessionID"].ToString()) == false)
+                {
+
+                    if (customerName == r["customerName"].ToString())
+                    {
+                        sessionID.Add(r["sessionID"].ToString());
+                        ProductName.Add(r["productName"].ToString());
+                        cost += decimal.Parse(r["price"].ToString(), System.Globalization.NumberStyles.Currency);
+                        quantity += int.Parse(r["quantity"].ToString());
+                        totalP.Text = "Total: $" + cost.ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Different Customer");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("This Item is already added");
+                }
+            }
+
+        }
+
+        private void OrderBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Cart_Page());
+            Show_Cart_Click(sender, e);
+        }
+
+        private void RemoveBtn_Click(object sender, RoutedEventArgs e)
         {
 
         }
